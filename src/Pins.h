874b0311b5b90535a47a6eeef6d72410234f235a
Niklas <niklas@nokxs.de>
2020-06-25 15:49:20 +0200

    Add Gpio Number to pins

--- src/Pins.h
+++ src/Pins.h
@@ -2,12 +2,12 @@
 #define Pins_h
 
 // Inputs
-#define PIN_BUTTON1_INPUT D1
-#define PIN_BUTTON2_INPUT D2
-#define PIN_BUTTON3_INPUT D5
-#define PIN_BUTTON4_INPUT D6
+#define PIN_BUTTON1_INPUT D1 // GPIO 05
+#define PIN_BUTTON2_INPUT D2 // GPIO 04
+#define PIN_BUTTON3_INPUT D5 // GPIO 14
+#define PIN_BUTTON4_INPUT D6 // GPIO 12
 
 // Outputs
-#define PIN_LED1 D7
+#define PIN_LED1 D7 // GPIO 13
 
 #endif
